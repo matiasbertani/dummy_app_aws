af0e5e00abc17f157e8c9be10222a576f0c1aab9
Matias Bertani <matiasbertani@gmail.com>
2023-03-21 12:36:24 -0300

    changing east for west

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "vpc-eks-tf"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = false
