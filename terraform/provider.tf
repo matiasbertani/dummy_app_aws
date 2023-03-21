61b366d9720fe6299f034a67999f93a6f07c9c9e
Matias Bertani <matiasbertani@gmail.com>
2023-03-21 10:12:43 -0300

    adding initial setup for terraform and aws

---
+++ terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "dummy-hello-app"
+}
