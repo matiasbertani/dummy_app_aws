169932992a597ca692a7ecd9b81cf97876bfd0a8
matiasbertani <matiasbertani@gmail.com>
2023-03-22 12:36:29 -0300

    deleting unneccesary configurations

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,22 +5,8 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.22"
 
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
-
-  node_security_group_additional_rules = {
-    ingress_allow_access_from_control_plane = {
-      type                          = "ingress"
-      protocol                      = "tcp"
-      from_port                     = 9443
-      to_port                       = 9443
-      source_cluster_security_group = true
-      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-    }
-  }
 
   eks_managed_node_groups = {
       group1 = {
@@ -31,7 +17,6 @@ module "eks" {
         instance_types = ["t3.large"]
       }
     }
-
 
   tags = {
     Terraform   = "true"
